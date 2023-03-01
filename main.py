7522666dac2c74d0bb7095c85cd4d6c40eaa85bd
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-03-01 10:08:46 -0300

    ok

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from pytube import YouTube, Playlist
 
-
 VIDEO_URL = 'https://www.youtube.com/watch?v=8EJ3zbKTWQ8&list=PLyORnIW1xT6waC0PNjAMj33FdK2ngL_ik'
 PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PLyORnIW1xT6waC0PNjAMj33FdK2ngL_ik'
-
 
 def download_video(video_url):
 	yt = YouTube(video_url)
@@ -21,7 +19,6 @@ def download_audio(video_url):
 	yt = YouTube(video_url)
 	audio = yt.streams.filter(only_audio=True)[0]
 	audio.download()
-
 
 if __name__ == '__main__':
 	download_video(VIDEO_URL)
