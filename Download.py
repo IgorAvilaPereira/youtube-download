341f7e6758f50a9b95a7f4e9ad6e2fa934939e41
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-04-06 15:57:06 -0300

    flet gui

---
+++ Download.py
@@ -0,0 +1,58 @@
+from pytube import YouTube, Playlist
+import os
+
+class Download:
+
+    # def on_progress(stream, data_chunk, bytes_remaining):
+    #     #print('bytes_remaining:', bytes_remaining)
+    #     downloaded = max_file_size - bytes_remaining
+    #     percent = downloaded/max_file_size
+    #     print(f'Downloaded: {percent:.0%}', end='\r')
+
+    # def on_complete(self, stream, path):
+    #     return True
+        
+
+    def baixarVideo(self, url_video):
+        # yt = YouTube(url_video, on_complete_callback=self.on_complete)
+        yt = YouTube(url_video)
+        video = yt.streams.get_highest_resolution()        
+        video.download(output_path='video')
+    def baixarPlaylist(self, url_playlist) :
+        # playlist = Playlist(url_playlist, on_complete_callback=self.on_complete)
+        playlist = Playlist(url_playlist)
+        for url_video in playlist:
+            yt = YouTube(url_video)
+            video = yt.streams.get_highest_resolution()
+            video.download(output_path='playlist')
+    def baixarAudio(self, url_video):
+        # yt = YouTube(url_video, on_complete_callback=self.on_complete)
+        yt = YouTube(url_video)
+        # audio = yt.streams.filter(only_audio=True)[0]
+        audio = yt.streams.filter(only_audio=True).first()
+        out_file = audio.download(output_path='audio')        
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+
+    def gerarLinksMarkdown(self, url_playlist):
+        f = open("playlist.md", "w")
+        conteudo = ""     
+        playlist = Playlist(url_playlist)
+        conteudo = "# Videos - "+playlist.title+"\n\n"
+        conteudo = conteudo + "* [Playlist]("+url_playlist+")\n\n"
+        for url_video in playlist:
+            yt = YouTube(url_video)
+            conteudo = conteudo + "* ["+url_video+"] \n\n"
+        f.write(conteudo)      
+        f.close()
+
+# if __name__ == '__main__':    
+#     download = Download()
+#     url_playlist = "https://youtube.com/playlist?list=PLvT8P1q6jMWfsGSRD3MP8cuR_gnsDD59F"
+#     download.gerarLinksMarkdown(url_playlist)
+#     # download.baixarPlaylist(url_playlist)
+#     # url_playlist = "https://youtube.com/playlist?list=PLvT8P1q6jMWfdK6v25Sjhq3qwxLIAnvtk"
+#     # download.baixarPlaylist(url_playlist)
+
+    
--- main.py
+++ main.py
@@ -1,26 +1,98 @@
-from pytube import YouTube, Playlist
+from Download import *
+import flet as ft
+import os
+import platform
+import subprocess
 
-VIDEO_URL = 'https://www.youtube.com/watch?v=8EJ3zbKTWQ8&list=PLyORnIW1xT6waC0PNjAMj33FdK2ngL_ik'
-PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PLyORnIW1xT6waC0PNjAMj33FdK2ngL_ik'
+# def open_file(path):
+#     if platform.system() == "Windows":
+#         os.startfile(path)
+#     elif platform.system() == "Darwin":
+#         subprocess.Popen(["open", path])
+#     else:
+#         subprocess.Popen(["xdg-open", path])
 
-def download_video(video_url):
-	yt = YouTube(video_url)
-	video = yt.streams.get_highest_resolution()
-	video.download()
+def main(page):
+    # page.window_title = "Youtube Downloader "
+    page.window_width = 600        # window's width is 200 px
+    page.window_height = 400       # window's height is 200 px
+    page.window_resizable = False  # window is not resizable
 
-def download_playlist(playlist_url):
-	playlist = Playlist(playlist_url)
-	for url in playlist:
-		yt = YouTube(url)
-		video = yt.streams.get_highest_resolution()
-		video.download(output_path='playlist')
+    def open_video_folder(e):                
+        path = "./video"    
+        if platform.system() == "Windows":
+            os.startfile(path)
+        elif platform.system() == "Darwin":
+            subprocess.Popen(["open", path])
+        else:
+            subprocess.Popen(["xdg-open", path])
 
-def download_audio(video_url):
-	yt = YouTube(video_url)
-	audio = yt.streams.filter(only_audio=True)[0]
-	audio.download()
+    def open_audio_folder(e):
+        path = "./audio"
+        if platform.system() == "Windows":
+            os.startfile(path)
+        elif platform.system() == "Darwin":
+            subprocess.Popen(["open", path])
+        else:
+            subprocess.Popen(["xdg-open", path])
 
-if __name__ == '__main__':
-	download_video(VIDEO_URL)
-	download_playlist(PLAYLIST_URL)
-	download_audio(VIDEO_URL)
+    def open_playlist_folder(e):
+        path = "./playlist"
+        if platform.system() == "Windows":
+            os.startfile(path)
+        elif platform.system() == "Darwin":
+            subprocess.Popen(["open", path])
+        else:
+            subprocess.Popen(["xdg-open", path])
+
+    def add_new_video(e):
+        url_video = new_video.value
+        download = Download()
+        download.baixarVideo(url_video)
+        page.add(ft.Checkbox(label=new_video.value))
+        new_video.value = ""
+        new_video.focus()
+        new_video.update()
+    
+    def add_new_playlist(e):
+        url_playlist = new_playlist.value
+        download = Download()
+        download.baixarPlaylist(url_playlist)
+        page.add(ft.Checkbox(label=new_playlist.value))
+        new_playlist.value = ""
+        new_playlist.focus()
+        new_playlist.update()
+
+    def export_playlist(e):
+        url_playlist = new_playlist.value
+        download = Download()
+        download.gerarLinksMarkdown(url_playlist)
+        new_playlist.value = ""
+        new_playlist.focus()
+        new_playlist.update()
+
+
+    def add_new_audio(e):
+        url_audio = new_audio.value
+        download = Download()
+        download.baixarAudio(url_audio)
+        page.add(ft.Checkbox(label=new_video.value))
+        new_audio.value = ""
+        new_audio.focus()
+        new_audio.update()
+
+    new_video = ft.TextField(hint_text="Youtube Video URL?", width=200)
+    page.add(ft.Row([new_video, ft.ElevatedButton("Download Video", on_click=add_new_video)]))
+
+    new_playlist = ft.TextField(hint_text="Youtube Playlist URL?", width=200)
+    page.add(ft.Row([new_playlist, ft.ElevatedButton("Download Playlist", on_click=add_new_playlist), ft.ElevatedButton("Export Playlist to Markdown", on_click=export_playlist)]))
+
+    new_audio = ft.TextField(hint_text="Youtube Video URL?", width=200)
+    page.add(ft.Row([new_audio, ft.ElevatedButton("Download Audio", on_click=add_new_audio)]))
+
+    page.add(ft.FilledButton(text="Open Video Folder", on_click=open_video_folder), 
+        ft.FilledButton(text="Open Playlist Folder", on_click=open_playlist_folder),
+        ft.FilledButton(text="Open Audio Folder", on_click=open_audio_folder)
+    )
+
+ft.app(target=main)
